472008dc53106881e3232885da84c6da40f55a5e
mac <91299542+Maclia@users.noreply.github.com>
2026-01-27 00:00:08 +0300

    Add TS shims for express/swagger and annotate req/res; include .d.ts in server tsconfig

---
+++ server/types/shims.d.ts
@@ -0,0 +1,22 @@
+// Minimal declaration shims for dependencies not installed in this environment
+declare module 'express' {
+  import type * as http from 'http';
+  export type Request = http.IncomingMessage & { body?: any; params?: any; query?: any };
+  export type Response = http.ServerResponse & { json?: (body: any) => void; status?: (code: number) => Response };
+  export type NextFunction = (err?: any) => void;
+  const express: any;
+  export default express;
+}
+
+declare module 'swagger-jsdoc' {
+  const swaggerJSDoc: any;
+  export default swaggerJSDoc;
+}
+
+declare module 'swagger-ui-express' {
+  const swaggerUi: any;
+  export default swaggerUi;
+}
+
+// Provide setImmediate when Node types are not installed
+declare var setImmediate: (callback: (...args: any[]) => void, ...args: any[]) => any;
