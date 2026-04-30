86ba251b03f37f8e0536b76197164324b46bd015
mac <91299542+Maclia@users.noreply.github.com>
2026-04-30 17:24:31 +0300

    Resolve for VIte

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
        }
     },
     // Optimize for production
-    minify: 'esbuild',
+    minify: 'oxc',
     target: 'es2020',
    chunkSizeWarningLimit: 900
    },
